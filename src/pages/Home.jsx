dc22bfdbf0fa9e2a805724d5e57e80e806029ea2
ingridduggan <duggan.ingrid@gmail.com>
2025-07-08 22:44:06 +0100

    fix toggle and adjust meteors for screen size

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { ToggleButton } from '../components/ThemeToggle'
 import { StarBackground } from '../components/StarBackground';
+import { NavBar } from '../components/NavBar';
 
 export const Home = () => {
     return <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
@@ -12,6 +13,7 @@ export const Home = () => {
         <StarBackground />
 
         {/*Navbar*/}
+        <NavBar />
 
         {/*Main Content*/}
         
